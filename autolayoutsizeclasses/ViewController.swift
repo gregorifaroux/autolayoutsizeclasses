835d5a221b63c9e0f29af43faeba7d3ba8ab8000
Gregori Faroux <gfaroux@Gregoris-iMac.dyn.gsu.edu>
2015-03-06 08:15:40 -0500

    Initial Commit

---
+++ autolayoutsizeclasses/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  autolayoutsizeclasses
+//
+//  Created by Gregori Faroux on 3/6/15.
+//  Copyright (c) 2015 digistarters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
